0b7c5da7fd2a30c51415d1e55a3e016f4d4f7d65
manivasagam <s.kishore123.64@gmail.com>
2022-09-13 12:23:54 +0530

    done

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-let res =  fetch("https://restcountries.com/v2/all");
-res.then((data)=>data.json()).then((value)=>{
-console.log(value);
 let container = document.createElement("div");
 container.setAttribute("class","container");
 let row = document.createElement("div");
 row.classList.add("row","m-3");
 container.append(row)
 document.body.append(container);
+
+let res =  fetch("https://restcountries.com/v2/all");
+res.then((data)=>data.json()).then((value)=>{
+console.log(value);
+
 
 
 // let divi = document.createElement("div");
@@ -25,15 +27,39 @@ for(let i =0;i<value.length;i++)
     <div>Capital:${value[i].capital}</div>
     <div>Region:${value[i].region}</div>
     <div>Country-Code:${value[i].alpha3Code}</div>
-     <a href="https://api.openweathermap.org/data/2.5/weather?q=${value[i].name}&appid=721c0db583c897dfe90fd7fad5f12c8f" class="card-link" target="_blank">Click here for weather</a>
+    
     </div>
     
     </div>`;
 
     let button = document.createElement("button");
     button.addEventListener("click",fun);
-    button.innerHTML="Clickhere"
+    button.innerHTML="Click for Temperture"
     div.append(button);
+
+    
+
+    async function fun(){
+
+    
+    let span = document.createElement("span");
+    span.setAttribute("id","span");
+    
+
+   let url= `https://api.openweathermap.org/data/2.5/weather?q=${value[i].name}&appid=721c0db583c897dfe90fd7fad5f12c8f`
+   let res= await fetch(url);
+    let res1= await res.json();
+    span.innerHTML=res1.main.temp;
+
+    var br = document.createElement("br");
+
+    div.append(br,span);
+  }
+
+
+
+
+
 row.append(div);
   
 }
@@ -45,7 +71,7 @@ row.append(div);
 
 
 function fun(name){
-  let weather=fetch(`https://api.openweathermap.org/data/2.5/weather?q=japan&appid=721c0db583c897dfe90fd7fad5f12c8f`)
+  let weather=fetch(`https://api.openweathermap.org/data/2.5/weather?q=&appid=721c0db583c897dfe90fd7fad5f12c8f`)
   weather.then((data)=>data.json()).then((we)=>{
   fu(we);});}
 
